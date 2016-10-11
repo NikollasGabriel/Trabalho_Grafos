e794e84718349716ba5d6af38e5fc206b01acc72
asus note <asus note@10.2.3.64>
2016-10-11 15:28:49 -0300

    Alterações na classe Vertice

--- src/grafos/Vertice.java
+++ src/grafos/Vertice.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Vertice {
 
     private String nomeVertice;
-    private ArrayList<Aresta> aresta = new ArrayList<>();
+    private ArrayList<Aresta> arestas = new ArrayList<>();
 
     public String getNomeVertice() {
         return nomeVertice;
@@ -13,6 +13,17 @@ public class Vertice {
 
     public void setNomeVertice(String nomeVertice) {
         this.nomeVertice = nomeVertice;
+    }
+    
+    void adcionaAresta (Aresta aresta){
+        arestas.add(aresta);
+    }
+    
+    void mostraArestas (Vertice vertice){
+       System.out.println("Vertice: "+vertice.getNomeVertice());
+        for (Aresta aresta : arestas) {
+            System.out.println("Vertices: "+aresta.getVertice().getNomeVertice());
+        }
     }
 
     public Vertice(String nomeVertice) {
@@ -27,17 +38,14 @@ public class Vertice {
 
 // <editor-fold defaultstate="collapsed" desc="Código útil.">
 /*void exibeLista(Vertice bleh){
-=======
     public Vertice(){};
     
     void exibeLista(Vertice bleh){
->>>>>>> 9cf279fb5d7095d8036e044c039460824210e43a
         
         System.out.println("Vertice: "+bleh.getNomeVertice());
         for (Vertice vertice : ponteiros) {
             System.out.println("Vertices: "+vertice.getNomeVertice());
         }
-<<<<<<< HEAD
     }*/
 //</editor-fold>
 
